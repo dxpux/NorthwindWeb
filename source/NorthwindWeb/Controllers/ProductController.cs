49504252ca868402615ead353a180dfef4d142a2
力及 <dxpux@hotmail.com>
2016-12-31 16:03:09 +0800

    前端資料透過 Ajax 存取

--- source/NorthwindWeb/Controllers/ProductController.cs
+++ source/NorthwindWeb/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
-﻿using NorthwindModel.Repository;
+﻿using NorthwindModel;
+using NorthwindModel.Repository;
 using NorthwindService.Interface;
+using NorthwindWeb.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,9 @@ using System.Web.Mvc;
 
 namespace NorthwindWeb.Controllers
 {
+    /// <summary>
+    /// 產品管理
+    /// </summary>
     public class ProductController : Controller
     {
         private readonly IProductService productService;
@@ -17,9 +22,26 @@ namespace NorthwindWeb.Controllers
             this.productService = productService;
         }
 
+        /// <summary>
+        /// 產品管理頁面
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             return View(this.productService.GetAll());
+        }
+
+        [HttpPost]
+        public JsonResult JsonGetProducts()
+        {
+            try
+            {
+                return Json(new ReContent<IEnumerable<Product>>() { Success = true, Data = this.productService.GetAll() });
+            }
+            catch (Exception ex)
+            {
+                return Json(new ReContent<string>() { Success = false, Message = ex.Message });
+            }
         }
     }
 }
